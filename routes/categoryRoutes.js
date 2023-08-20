5a24193d17bd758adbb277598537d89f4addf1ae
Dedenno17 <ahmaddedensetiawan98@gmail.com>
2023-08-20 07:03:11 +0700

    feat: create category api

---
+++ routes/categoryRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const { getAllCategory, createCategory } = require("../controllers/category");
+const { verifyJWT } = require("../middleware/verifyJWT");
+
+// CREATE ROUTES
+const router = express.Router();
+
+// router.use(verifyJWT);
+
+// ROUTES
+router.get("/", getAllCategory);
+router.post("/", createCategory);
+
+module.exports = router;
